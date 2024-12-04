0b2381f1d0cb0e471de407e9eeb6e057e51b326b
Dennis Evangelista <devangel77b@gmail.com>
2024-12-03 19:07:43 -0500

    edits

--- krasnopolsky.R
+++ krasnopolsky.R
@@ -11,8 +11,12 @@ apredicted.ms2 <- function(m2,m1,mc){
   9.81*m2/(m2+m1+mc)
 }
 
+tpredicted.s <- function(m2,m1,mc,d){
+  (2*d/(9.81*m2/(m2+m1+mc)))^0.5
+}
+
 fig <- ggplot(data)+geom_point(aes(x=m2.kg,y=ameas.ms2))+
-    #ylim(0,11)+xlim(0,1.5)+
+    ylim(0,11)+
     geom_function(fun=apredicted.ms2,args=list(m1=0.0,mc=0.5),color='blue')+
     xlab('$m_2$ (\\unit{\\kilo\\gram})')+
     ylab('$a$ (\\unit{\\meter\\per\\second\\squared})')+
@@ -21,6 +25,18 @@ fig <- ggplot(data)+geom_point(aes(x=m2.kg,y=ameas.ms2))+
 library(svglite)
 svglite('fig4.svg',width=3,height=2,pointsize=8)
 print(fig)
+dev.off()
+
+fig3 <- ggplot(data)+geom_point(aes(x=m2.kg,y=t.s))+
+    #ylim(0,11)+xlim(0,1.5)+
+    ylim(0,0.6)+
+    geom_function(fun=tpredicted.s,args=list(d=0.45,m1=0.0,mc=0.5),color='blue')+
+    xlab('$m_2$ (\\unit{\\kilo\\gram})')+
+    ylab('$t$ (\\unit{\\second})')+
+    theme_bw(base_size=8)
+
+svglite('fig3.svg',width=3,height=2,pointsize=8)
+print(fig3)
 dev.off()
 
 library(xtable)
